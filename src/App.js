f110d20fb6856f3fb448afbcf70602acbfb4e5f0
FrederikBRoth <32262672+FrederikBRoth@users.noreply.github.com>
2020-10-11 23:57:04 +0200

    prototype works

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Settings from "./components/Settings"
 
@@ -10,17 +10,18 @@ import {
 import './App.css';
 
 function App() {
-  function handleClick(){
+
+  function handleClick() {
     const audio = document.getElementById("audio")
+    console.log(audio)
+    audio.load()
     audio.play()
   }
 
   return (
     <div className="main" id="main" onClick={handleClick}>
       <Router>
-        <audio id="audio">
-          <source src="http://192.168.1.71:5000/9mF3P_9ORB_pfu_r192L1Qf18pwh0_qA.mp3" type="audio/mpeg" />
-        </audio>
+
         {/* <img class="dance-gif" src={require("./rats.gif")} alt="Family dancing to son's happy birthday song"/> */}
 
         <Switch>
--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 
+const apiPath = "http://192.168.1.71:5000/api";
+
+const postHeader = {
+    "Content-Type": "application/json"
+};
+
 function Settings() {
     let { test } = useParams()
     const history = useHistory()
     const [settingsOpen, setSettingsOpen] = useState(false)
     const [settingsHover, setSettingsHover] = useState(false)
     const [name, setName] = useState("Michael")
+    const [audioName, setAudioName] = useState("aOhOTevlJU_NscKxZCVzKZEjkKRcDGAK.mp3")
 
     function handlePopupClick() {
         setSettingsOpen(settingsOpen ? false : true);
@@ -22,10 +29,46 @@ function Settings() {
         }
     }
 
-    function handleKeyPress(event) {
+    async function handleKeyPress(event) {
         if (event.key === 'Enter') {
+            const data = await postName(name)
+            var audio = document.getElementById('audio');
+            var source = document.getElementById('audio-source');
+            const response = await polling(data)
+            console.log(response)
+            source.src = "http://192.168.1.71:5000/" + data
+            console.log(source.src)
+            // source.src = "http://192.168.1.71:5000/c0PHe1dLOQaCdOhShj6-htAyy9Q1UYWC.mp3"
+            audio.load()
+            audio.play()
             history.push("/" + name)
         }
+    }
+
+    async function polling(data) {
+        const response = await fetch("http://192.168.1.71:5000/" + data, {
+            cache: "no-store"
+        })
+
+        if (response.ok) {
+            return response
+        } else {
+            await polling(data)
+        }
+
+
+    }
+    async function postName(name) {
+        let response = await fetch(apiPath, {
+            method: "post",
+            headers: postHeader,
+            body: JSON.stringify({
+                name
+            })
+        });
+        let data = await response.text()
+
+        return data
     }
 
     function handleHover() {
@@ -50,7 +93,9 @@ function Settings() {
     }, [])
     return (
         <div className="settings-parent" id="settings-parent" onMouseEnter={handleHover} onMouseLeave={handleHover}>
-
+            <audio id="audio" >
+                <source src="" id="audio-source" />
+            </audio>
             <div className="settings-window">
                 <div className="settings">
                     <input type="text" className="settings-text" name="name" onChange={handleChange} id="settings-text" onKeyPress={handleKeyPress}></input>
